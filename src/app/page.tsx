f3e34e706485302251f7aa327966f4711e199daf
Eduardo-DevJs <dudev2001@gmail.com>
2024-05-08 17:45:17 -0300

    feat - buscando jogo no server component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import Container from "@/components/Container";
 import { GameProps } from "@/utils/types/game";
 import Image from "next/image";
 import Link from "next/link";
+import { BsArrowRightSquare } from "react-icons/bs";
 
 async function getDalyGame() {
   try {
     const response = await fetch(
-      `${process.env.NEXT_API_URL}/next-api/?api=game_day`
+      `${process.env.NEXT_API_URL}/next-api/?api=game_day`,
+      { next: { revalidate: 320 } }
     );
     return response.json();
   } catch (err) {
@@ -24,14 +26,23 @@ export default async function Home() {
         </h1>
         <Link href={`/game/${dalyGame.id}`}>
           <section className="w-full bg-black rounded-lg">
-            <Image
-              src={dalyGame.image_url}
-              alt={dalyGame.title}
-              priority={true}
-              quality={100}
-              width={100}
-              height={100}
-            />
+            <div className="w-full max-h-96 h-96 relative rounded-lg">
+              <div className="absolute z-20 bottom-0 p-3 flex justify-center items-center gap-2">
+                <p className="font-boold text-xl text-white">
+                  {dalyGame.title}
+                </p>
+                <BsArrowRightSquare size={24} color="#fff" />
+              </div>
+              <Image
+                src={dalyGame.image_url}
+                alt={dalyGame.title}
+                priority={true}
+                quality={100}
+                fill={true}
+                className="max-h-96 object-cover rounded-lg opacity-50 hover:opacity-100 transition-all duration-300"
+                sizes="(max-width: 768px) 100vw, (max-width: 1280px) 33vw"
+              />
+            </div>
           </section>
         </Link>
       </Container>
